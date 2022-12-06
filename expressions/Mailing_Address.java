85740db47b9d21a81bd1dc2c5bf6a9bad467f97a
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-06 15:10:32 +0530

    Create Mailing_Address.java

---
+++ expressions/Mailing_Address.java
@@ -0,0 +1,10 @@
+package expressions;
+
+public class Mailing_Address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sushmeetha" +"\n"+ "Flat 101,Park View Building,Satyam Heights,Rajiv Gandhi Nagar,Bachupally,500090 ");
+	}
+
+}
