f058fd6a8ae1c8579e5d6d7632da89325bdf9b33
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-15 22:55:43 -0500

    Create Area_of_a_room.java

---
+++ expressions/Area_of_a_room.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class AreaOfRoom {
+
+	public static void main(String[] args) {
+		System.out.println("enter width and length of your room");
+		Scanner s = new Scanner(System.in);
+		float w = s.nextFloat();
+		float l = s.nextFloat();
+		float area = w*l;
+		System.out.println("the area of room is " +area );
+		
+		
+		
+
+	}
+
+}
