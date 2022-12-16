6526f3c017806b22fccc45b825aa677883ceb8e1
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-15 22:53:10 -0500

    Create HelloUser.java

---
+++ expressions/HelloUser.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class HelloUser {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name");
+		Scanner s = new Scanner(System.in);
+		String name = s.next();
+		System.out.println("Hello " + name);
+		
+		
+		
+		
+
+	}
+
+}
