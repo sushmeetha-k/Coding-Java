830dc312393978042dce1d635aa550ff5f42c555
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-21 13:58:48 +0530

    Create Right_Triangle_Pattern.java

---
+++ Repetitions/Nested Loops/Right_Triangle_Pattern.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Right_Triangle_Pattern {
+
+	public static void main(String[] args) {
+		System.out.println("enter n");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
