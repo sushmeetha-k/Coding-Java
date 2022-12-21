d06b97a291a18cc84713291268eb635351c04ad4
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-21 14:02:55 +0530

    Create Multiplication.java

---
+++ Repetitions/Nested Loops/Multiplication.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		System.out.println("\t"+1+"\t"+2+"\t"+3+"\t"+4+"\t"+5+"\t"+6+"\t"+7+"\t"+8+"\t"+9+"\t"+10);
+		for(int i=1;i<=10;i++){
+			System.out.print(i+"\t");
+			for(int j=1;j<=10;j++){
+				System.out.print(i*j+"\t");
+			}
+		System.out.println();
+	}
+		
+	}
+	}
