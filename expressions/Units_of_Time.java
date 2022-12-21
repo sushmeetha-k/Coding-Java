20d8ccf0429b7a054bbcf5b233f719704bdff5f5
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-21 13:18:36 +0530

    Create Units_of_Time.java

---
+++ expressions/Units_of_Time.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class UnitsOfTime {
+
+	public static void main(String[] args) {
+		System.out.println("Enter number of seconds");
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		
+		
+
+	}
+
+}
