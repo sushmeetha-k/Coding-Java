932cb2d43fd3aac5d5d8ea4feb03bb7f1bd7c6f8
sushmeetha-k <108419902+sushmeetha-k@users.noreply.github.com>
2022-12-21 13:20:45 +0530

    Create Sum_of_positive.java

---
+++ expressions/Sum_of_positive.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Sum_of_positive {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a positive integer");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int sum = n*(n+1)/2;
+		System.out.println("sum of integers from 1 to "+ n + " is: "+sum);
+
+	}
+
+}
+Footer
+© 20
